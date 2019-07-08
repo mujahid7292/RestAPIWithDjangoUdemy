8396528a8be03b3696dd308fe2174c72e04ae3e7
Saifullah Al Mujahid <mujahid7292@gmail.com>
2019-07-08 17:37:55 +0600

    Finish Branch

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,14 @@
 
-def add(x,y):
+def add(x, y):
     """
     This function add two number together.
     """
     return x + y
+
+
+def substract(x, y):
+    """
+    This function will substract y from x & will
+    return the substracted value
+    """
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,19 +1,28 @@
 from django.test import TestCase
 # This above import will help us to test our django app.
 
-#Next we will import function which we want to test.
+# Next we will import function which we want to test.
 from app.calc import add
+from app.calc import substract
 
-#Now we will create a classs which will test our calc
-#class.
+# Now we will create a classs which will test our calc
+# class.
+
+
 class CalcTest(TestCase):
-    #We are going to inherit this class from TestCase
+    # We are going to inherit this class from TestCase
 
     # Now we will create test function to test the add
-    #function.
+    # function.
 
     def test_add_numbers(self):
         """
         Test that two numbers are added together.
         """
-        self.assertEqual(add(3,8),11)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_substract_numbers(self):
+        """
+        Test that vlues are substracted and returned
+        """
+        self.assertEqual(substract(11, 5), 6)
