f4a94b0fa8f883759370235c88d9350363836341
Saifullah Al Mujahid <mujahid7292@gmail.com>
2019-07-14 17:26:06 +0600

    setUp function finish

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,36 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        """
+        This function is run before every test from this
+        class is run. So some times there are some set up
+        task, that need to be done before running every
+        test from this class.
+        """
+        # First create a test client
+        self.client=Client()
+        # What this above line does is, it basically set
+        # Client() to self, so that other function from
+        # this class can access Client()
+
+        # Create an admin, which will be used to logged
+        # into the above client
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        # Now make the admin login to the client
+        self.client.force_login(self.admin_user)
+        # This above line will help us to run our test smothly,
+        # because now we don't have to manualy log the user in,
+        # we can use this above force_login() helper function.
+
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='Test123',
+            name='Full name of the test user'
+        )
