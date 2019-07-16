297eb3ee42272543369db73fad3526f148d70c2e
Saifullah Al Mujahid <mujahid7292@gmail.com>
2019-07-16 17:09:01 +0600

    Finish: 30.ModifyDjangoAdminToListOurCustomUserModel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """
+    This our custom user admin.
+    """
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+# Now we need to register our UserAdmin class
+admin.site.register(models.User, UserAdmin)
